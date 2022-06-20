6616a7246c00f8972ddc917784b4c8abfa0c2fce
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-06-20 22:07:47 +0900

    데이터 바인딩

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,31 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <div>
+    <h4>{{ products[0] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[1] }}</h4>
+    <p>50 만원</p>
+  </div>
+  <div>
+    <h4>{{ products[2] }}</h4>
+    <p>50 만원</p>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// vue의 실시간 자동 렌더링 기능을 사용하고 싶으면 {{}}를 사용해라.
+// data를 변경함녀 data와 관련된 HTML도 실시간으로 반영된다.
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
+    };
+  },
+  components: {},
+};
 </script>
 
 <style>
