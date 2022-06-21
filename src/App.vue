f7f9aeff94efc7b387e9e8f435098d71e20b5e99
yooss2006 <68742190+yooss2006@users.noreply.github.com>
2022-06-21 22:48:03 +0900

    v-for, v-if, event

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <div>
-    <h4>{{ products[0] }}</h4>
-    <p>50 만원</p>
+  <div class="black-bg" v-if="isModal" @click="isModal = false">
+    <div class="white-bg">
+      <h4>상세페이지</h4>
+      <p>상세페이지내용임</p>
+    </div>
   </div>
-  <div>
-    <h4>{{ products[1] }}</h4>
-    <p>50 만원</p>
+  <div class="menu">
+    <a v-for="(a, index) in menus" :key="index">{{ a }}</a>
   </div>
-  <div>
-    <h4>{{ products[2] }}</h4>
+  <div v-for="(a, i) in products.length" :key="i">
+    <img src="./assets/room1.jpg" />
+    <h4 @click="isModal = true">{{ products[i] }}</h4>
     <p>50 만원</p>
+    <button @click="increase(i)">허위매물신고</button>
+    <span>신고 수 : {{ number[i] }}</span>
   </div>
 </template>
 <script>
-// vue의 실시간 자동 렌더링 기능을 사용하고 싶으면 {{}}를 사용해라.
-// data를 변경함녀 data와 관련된 HTML도 실시간으로 반영된다.
-
 export default {
   name: "App",
   data() {
     return {
+      isModal: false,
+      number: [0, 0, 0],
+      menus: ["Home", "Shop", "About"],
       products: ["역삼동원룸", "천호동원룸", "마포구원룸"],
     };
+  },
+  methods: {
+    increase(i) {
+      this.number[i] += 1;
+    },
   },
   components: {},
 };
 </script>
 
 <style>
+body {
+  margin: 0;
+}
+div {
+  box-sizing: border-box;
+}
+.black-bg {
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed;
+  padding: 20px;
+}
+.white-bg {
+  width: 100%;
+  background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.menu {
+  background: darkslateblue;
+  padding: 15px;
+  border-radius: 5px;
+}
+.menu a {
+  color: white;
+  padding: 10px;
 }
 </style>
